ae7f96537428a926f69f9c8fec3c13e68cd0c4fb
Amri Karisma <amrikarisma@yahoo.com>
2020-12-27 00:17:46 +0700

    initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+import React from 'react';
+
+
+const App = () => {
+    return (
+        <View>
+            <Text>Ini Aplikasi React Native Pertama Saya</Text>
+        </View>
+    );
+}
+export default App;
